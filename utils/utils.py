bea1eeceb8c7b0e2de448c500db075bdd3067f43
lurovi <luigirovito2@gmail.com>
2024-02-17 18:58:50 +0100

    Replaced lambda functions with global-scope defined functions for avoiding pickle problems with multiprocessing

--- utils/utils.py
+++ utils/utils.py
@@ -4,7 +4,7 @@ import enum
 import numpy as np
 import torch
 import yaml
-
+from functools import partial
 from gp.prog_eval import ProgramEvaluator
 
 
@@ -37,9 +37,9 @@ def cubic_seed(base_seed: int, offset_seed: int, multiply_by_prime_number: bool 
 def make_fit_fun_gp(x, y, opcodes, reduction='mean'):
     pg_eval: ProgramEvaluator = ProgramEvaluator(opcodes)
     if reduction == 'mean':
-        return lambda prg: torch.mean((y - pg_eval(x, prg)) ** 2)
+        return partial(fit_fun_gp_mean, x=x, y=y, pg_eval=pg_eval)
     if reduction == 'sum':
-        return lambda prg: torch.sum((y - pg_eval(x, prg)) ** 2)
+        return partial(fit_fun_gp_sum, x=x, y=y, pg_eval=pg_eval)
 
 
 def make_fit_fun_gpgd(x, y, reduction='mean'):
@@ -47,9 +47,25 @@ def make_fit_fun_gpgd(x, y, reduction='mean'):
     fitness function for gpgd routine
     """
     if reduction == 'mean':
-        return lambda prg: torch.mean((y - prg(x)) ** 2)
+        return partial(fit_fun_gpgd_mean, x=x, y=y)
     elif reduction == 'sum':
-        return lambda prg: torch.sum((y - prg(x)) ** 2)
+        return partial(fit_fun_gpgd_sum, x=x, y=y)
     else:
-        return lambda prg: torch.mean((y - prg(x)) ** 2)
+        return partial(fit_fun_gpgd_mean, x=x, y=y)
+
+
+def fit_fun_gp_mean(prg, x, y, pg_eval):
+    return torch.mean((y - pg_eval(x, prg)) ** 2)
+
+
+def fit_fun_gp_sum(prg, x, y, pg_eval):
+    return torch.sum((y - pg_eval(x, prg)) ** 2)
+
+
+def fit_fun_gpgd_mean(prg, x, y):
+    return torch.mean((y - prg(x)) ** 2)
+
+
+def fit_fun_gpgd_sum(prg, x, y):
+    return torch.sum((y - prg(x)) ** 2)
 
